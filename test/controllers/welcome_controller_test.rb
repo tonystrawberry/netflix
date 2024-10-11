55a59711d69de0c8891f462a5003e55d45716695
Tony Duong <tony.duong.102@gmail.com>
2024-10-12 08:15:14 +0900

    :green_heart: fix CI alerts

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_url
+    get welcome_url(locale: "en")
     assert_response :success
   end
 end
